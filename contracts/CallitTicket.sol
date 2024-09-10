585c049afcaa28c5bb99fe7d32efcb034f2a2fe3
house <housesellout@gmail.com>
2024-09-10 14:16:54 -0400

    :- bug fix attempt with ticket (removed ‘Ownable’) 	- replaced Ownable inheritance with simple modifier onlyVault 	- hoping there was an issue with setting owner of a contract to the contract itself (address(this)) 		and then invoking transferOwnership from the contract to another contract 		… i dunno 	- ready to deploy config w/ new ticket compile 		then do config migration of old contracts 	- everything still compiles just fine

--- contracts/CallitTicket.sol
+++ contracts/CallitTicket.sol
@@ -16,7 +16,7 @@ pragma solidity ^0.8.24;
 
 // local _ $ npm install @openzeppelin/contracts
 import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
-import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
+// import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 interface ICallitConfig { // don't need everything in ICallitConfig.sol
     function ADDR_VAULT() external view returns(address);
@@ -26,40 +26,47 @@ interface ICallitVault {
     function deposit(address _depositor) external payable;
 }
 
-contract CallitTicket is ERC20, Ownable {
-    string public tVERSION = '0.5';
-    address public ADDR_CONFIG; // set via constructor()
-    ICallitConfig private CONF = ICallitConfig(ADDR_CONFIG);
-
+contract CallitTicket is ERC20 {
     // address public constant TOK_WPLS = address(0xA1077a294dDE1B09bB078844df40758a5D0f9a27);
     // address public constant BURN_ADDR = address(0x0000000000000000000000000000000000000369);
-    // address public ADDR_VAULT;
-    // address public ADDR_FACT;
+    string public tVERSION = '0.5';
+    address public ADDR_CONFIG; // set via constructor()
+    ICallitConfig private CONF; // set via constructor()
     event MintedForPriceParity(address _receiver, uint256 _amount);
     event BurnForWinLoseClaim(address _account, uint256 _amount);
 
-    // constructor(uint256 _initSupply, address _vault, address _fact, string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(_vault) {
-    //     VAULT_ADDR = _vault;
-    //     FACT_ADDR = _fact;
-    //     // NOTE: uint64 = ~18,000Q max
-    //     _mint(VAULT_ADDR, _initSupply * 10**uint8(decimals())); // 'emit Transfer'
-    // }
-    constructor(uint256 _initSupply, string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(address(this)) {
+    constructor(uint256 _initSupply, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         ADDR_CONFIG = msg.sender; // config invokes new CallitTicket(...)
         CONF = ICallitConfig(ADDR_CONFIG);
         address vault = CONF.ADDR_VAULT();
-            // LEFT OFF HERE .. still failing here ^ with latest deploy
-            // CallitConfig v0.11
-        require(vault != address(0), ' !vault :7 '); // sanity check (msg.sender is a VAULT)
-        transferOwnership(vault);
+
+        // sanity check: access vault from msg.sender (ie. CONF)
+        require(vault != address(0), ' !vault :7 '); 
+
+        // mint to vault (vault creates and maintains LPs)
         _mint(vault, _initSupply * 10**uint8(decimals())); // 'emit Transfer'
+        
+        // NOTE: no way to change/update CONF after deployment (there will be too many)
+        //  this results in ...
+        //      only OG factory can burn (ie. claim winnings breaks, if factory changed)
+        //      only OG vault can mint (ie. exe arb price parity breaks, if vault changed)
+        //      OG vault gets fwd all 'receive()' deposits (not such a big deal)
+        //  NOTE: this means that if factory or vault addresses ever need to be changed
+        //      then all previously created tickets under them, will break
+        //
+        //      HOWEVER, there may be a work around since we are storing 
+        //          ticket addies in struct MARKET.MARKET_RESULTS
+    }
+    modifier onlyVault() {
+        require(msg.sender == CONF.ADDR_VAULT(), " !vault ;p ");
+        _;        
     }
     modifier onlyFactory() {
         require(msg.sender == CONF.ADDR_FACT(), " !fact ;p ");
         _;
     }
     // inovked by vault
-    function mintForPriceParity(address _receiver, uint256 _amount) external onlyOwner() {
+    function mintForPriceParity(address _receiver, uint256 _amount) external onlyVault() {
         _mint(_receiver, _amount);
         emit MintedForPriceParity(_receiver, _amount);
     }
@@ -69,12 +76,9 @@ contract CallitTicket is ERC20, Ownable {
         emit BurnForWinLoseClaim(_account, balanceOf(_account));
     }
 
-    // NOTE: no way to change/update CONF after deployment
-
-    // fwd any PLS recieved to VAULT (convert to USD stable & process deposit)
+    // process PLS value received
     receive() external payable {
-        // process PLS value sent
-        uint256 amntIn = msg.value;
-        ICallitVault(CONF.ADDR_VAULT()).deposit{value: amntIn}(msg.sender);
+        // fwd any PLS recieved to VAULT (convert to USD stable & process deposit)    
+        ICallitVault(CONF.ADDR_VAULT()).deposit{value: msg.value}(msg.sender);
     }
 }
