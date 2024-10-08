b20be62c82704cc065e56ee033466ca689017f7e
house <housesellout@gmail.com>
2024-10-08 00:51:00 -0400

    :- DEPLOYED new factory v0.72 (another attempt at fixing close market)	- decoupled percent of function from deduct fee percent function 		now not using deduct fee percent, and just calc directly in factory 		but still using LIB._perc of function 	- updated _setContracts 	- tested and still failed unfortunately :/ 		… currently debugging, again
    latest deploys…
    #-----------------------------------------------------------------------------------------------------------#
    address public ADDR_LIB = address(0x437dedd662736d6303fFB7ACd321966f4a81da3d); // CallitLib v0.32
    address public ADDR_VAULT = address(0xc82D3e9Ed0B92EF0a6273090DC7F79EF2F53ACa4); // CallitVault v0.53
    address public ADDR_DELEGATE = address(0xB4300bCdE9BE07B3057C36D1F05BBb8F0D0128b8); // CallitDelegate v0.50
    address public ADDR_CALL = address(0x200F9C731c72Dce8974B28B52d39c20381efb37e); // CallitToken v0.21
    address public ADDR_FACT = address(0x26477F11AE1bAD4a97045217C03251Be25E43fCa); // CallitFactory v0.72
    address public ADDR_VOTER = address(0x01b5DC472bd537A95f5a51672902560610802d9c); // CallitVoter v0.3
    address public ADDR_CONFM = address(0x0718a6271A36D5cc9Fc9cE3e994A0A64F9611EC0); // CallitMarket v0.6
    address public ADDR_CONF = address(0xc5FB01Dea1e819bFcfF1690a2ffA493fDfeFae32); // CallitConfig v0.23

--- contracts/CallitFactory.sol
+++ contracts/CallitFactory.sol
@@ -60,7 +60,7 @@ contract CallitFactory {
     // address public constant BURN_ADDR = address(0x0000000000000000000000000000000000000369);
     
     /* GLOBALS (CALLIT) */
-    string public tVERSION = '0.71';  
+    string public tVERSION = '0.72';
     bool private FIRST_ = true;
     address public ADDR_CONFIG; // set via CONF_setConfig
     ICallitConfig private CONF; // set via CONF_setConfig
@@ -405,8 +405,12 @@ contract CallitFactory {
         // calc & set net prize pool after taking out voter reward pool (+ other market close fees)
         // mark.marketUsdAmnts.usdAmntPrizePool_net = mark.marketUsdAmnts.usdAmntPrizePool - mark.marketUsdAmnts.usdVoterRewardPool; // NOTE: write to market
         // mark.marketUsdAmnts.usdAmntPrizePool_net = LIB._deductFeePerc(mark.marketUsdAmnts.usdAmntPrizePool_net, CONF.PERC_MARKET_CLOSE_FEE(), mark.marketUsdAmnts.usdAmntPrizePool); // NOTE: write to market
-        uint64 usdAmntPrizePool_net_ = mark.marketUsdAmnts.usdAmntPrizePool - mark.marketUsdAmnts.usdVoterRewardPool; // NOTE: write to market
-        mark.marketUsdAmnts.usdAmntPrizePool_net = LIB._deductFeePerc(usdAmntPrizePool_net_, CONF.PERC_MARKET_CLOSE_FEE(), mark.marketUsdAmnts.usdAmntPrizePool); // NOTE: write to market
+        // uint64 usdAmntPrizePool_net_ = mark.marketUsdAmnts.usdAmntPrizePool - mark.marketUsdAmnts.usdVoterRewardPool;
+        // mark.marketUsdAmnts.usdAmntPrizePool_net = LIB._deductFeePerc(usdAmntPrizePool_net_, CONF.PERC_MARKET_CLOSE_FEE(), mark.marketUsdAmnts.usdAmntPrizePool); // NOTE: write to market
+        uint64 usdAmntPrizePool_net_ = mark.marketUsdAmnts.usdAmntPrizePool - mark.marketUsdAmnts.usdVoterRewardPool;
+        uint64 usdMarkCloseFee = LIB._perc_of_uint64(CONF.PERC_MARKET_CLOSE_FEE(), mark.marketUsdAmnts.usdAmntPrizePool);
+        mark.marketUsdAmnts.usdAmntPrizePool_net = usdAmntPrizePool_net_ - usdMarkCloseFee; // NOTE: write to market
+        
             // LEFT OFF HERE .. latest failed here with enum error
             //  0x4e487b710000000000000000000000000000000000000000000000000000000000000012
             //  Panic errors use the selector 0x4e487b71 and the following codes:
